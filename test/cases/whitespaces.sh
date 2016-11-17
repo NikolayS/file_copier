b1c228d5dfd58fa5b1308cd63b4f768709204c1f
NikolayS <github2@samokhvalov.com>
2016-11-17 20:02:24 +0300

    file URLs with whitespaces are not supported #34

---
+++ test/cases/whitespaces.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# to run this scenario, COPIERSERVICE should be defined (`export COPIERSERVICE=https://your.copier.host.name`)
+
+http GET "$COPIERSERVICE/?wh=1&md5=1&src=https://3.bp.blogspot.com/-eICF8Px0Eyc/WBBnc6vI51I/AAAAAAAANac/pxfFy2YvTfcl09MgwQ_aHO-ZHHR7XakJgCLcB/s640/Doar%20o%20lingurita%20din%20aceasta%20apa%20de%20gura%20facuta%20in%20casa%20te%20scapa%20de%20halena,%20carii%20si%20paradontoza.....jpg" -h 
+
